643fcca773634c810fcaa29b93270cd31406a5fc
Paulo Orquillo <paulo.orquillo@gmail.com>
2024-07-15 22:13:09 +1200

    without drag and drop

--- app/ui/board.tsx
+++ app/ui/board.tsx
@@ -4,10 +4,9 @@ import Square from "./square";
 import { Position } from "../page";
 
 interface BoardProps {
-
     knightPosition: Position;
     setKnightPosition: React.Dispatch<React.SetStateAction<Position>>;
-  }
+}
 
 export default function Board(props : BoardProps) {
     const squares = []
@@ -31,10 +30,23 @@ export default function Board(props : BoardProps) {
         );
     }
 
-
+    //handle onclick and set the knight new position
     function handdleSquareClick(toX: number, toY: number) {
-        setKnightPosition({knightX: toX, knightY: toY})
+        if (canMoveKnight(toX, toY)) {
+            setKnightPosition({knightX: toX, knightY: toY})
+        }
     }
+    // check if knight can move in that position
+    function canMoveKnight(toX: number, toY: number) {
+        const { knightX, knightY } = knightPosition
+        const dx = toX - knightX
+        const dy = toY - knightY
+      
+        return (
+          (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
+          (Math.abs(dx) === 1 && Math.abs(dy) === 2)
+        )
+      }
 
     for (let i = 0; i < 64; i++) {
         squares.push(renderSquare(i, knightPosition.knightX, knightPosition.knightY))
